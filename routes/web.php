b83864bdd9b2bfdc99e32e5c09adb38883379676
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-12-19 16:06:20 +0630

    - add system status message

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pdf');
+    return response()->json(['message'=>'Simple-Expense API is online.']);
 })->name('home');
